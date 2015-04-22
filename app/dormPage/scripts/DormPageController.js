71e2230ccda17d590cd7c9f501327db480515ea8
mingyueding <mingyueding2015@u.northwestern.edu>
2015-04-21 21:36:42 -0500

    style and add ratings

--- app/dormPage/scripts/DormPageController.js
+++ app/dormPage/scripts/DormPageController.js
@@ -17,8 +17,8 @@ angular
             $scope.dormAddrCode = $sce.trustAsResourceUrl(res.attributes.addrCode);
             $scope.ratelist = [{title:'Socialness', rate:res.attributes.socialness},
                 {title:'Room size',rate:res.attributes.roomsize},
-                {title:'Rowdiness',rate:res.attributes.rowdiness},
-                {title:'Bathrooms',rate:res.attributes.bathrooms},
+                {title:'Food',rate:res.attributes.Food},
+                {title:'Amenities',rate:res.attributes.Amenities},
                 {title:'Overall',rate:res.attributes.star}];
         });
 
